5e44eec7f975b137c443ea1e35cc96f804988520
Princewill <princesolo919@gmail.com>
2022-03-29 15:41:40 +0100

    checking for errors

--- AlgorithTest.js
+++ AlgorithTest.js
@@ -2,10 +2,13 @@
 
 // QUESTION ONE : BUYING AND SELLING PRICE FOR STOCK VALUE FOR COMPANY X
 
-// The values in the array represent the cost of a stock each day. As we can buy and sell the stock only once, we need to find the best buy and sell prices for which our profit is maximized
+// The values in the array represent the cost of a stock each day.
+//  As we can buy and sell the stock only once, we need to find the 
+// best buy and sell prices for which our profit is maximized
 
+//find local Minima and Maxima
 
-let BuySellday = function (price, n) {
+function BuySellday(price, n) {
 
     // Prices must be given for at least two days
     if (n == 1)
@@ -16,22 +19,12 @@ let BuySellday = function (price, n) {
 
     while (i < n - 1) {
 
-        // Find Local Minima
-        // Note that the limit is (n-2) as we are
-        // comparing present element to the next element
         while ((i < n - 1) && (price[i + 1] <= price[i]))
             i++;
         // If we reached the end, break
-        // as no further solution possible
         if (i == n - 1)
             break;
-
-        // Store the index of minima
         let buy = i++;
-
-        // Find Local Maxima
-        // Note that the limit is (n-1) as we are
-        // comparing to previous element
         while ((i < n) && (price[i] >= price[i - 1]))
             i++;
 
